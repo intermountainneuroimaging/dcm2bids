6084f04506ba4c9d8e9a18cb6c59a7b84b1eaa89
amyhegarty <76706603+amyhegarty@users.noreply.github.com>
2021-10-27 10:52:50 -0600

    Create run_dcm2niix.sh

---
+++ code/templates/run_dcm2niix.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+# load required modules...
+module use /projects/ics/modules
+module load fsl/6.0.3
+module load dcmtk 
+export DCMDICTPATH=/projects/ics/software/dcmtk/share/dcmtk/dicom.dic
+module load freesurfer/7.1.0
+module load dcm2niix/v1.0.20190410
+
+
+# Organize variables
+inputdicomdir=DICOM_PLACEHOLDER
+bidsfile=BIDSFILE_PLACEHOLDER
+tempfile=NIFTI_PLACEHOLDER
+
+tmpdir=`dirname $tempfile`
+mkdir -p $tmpdir
+log=$tempfile.log
+cmd="dcm2niix -z y -b y -f %d y -o $tmpdir -v y $inputdicomdir"
+echo $cmd >> $log
+$cmd >> $log 2>&1
+
+
+# after converting to nifti move to bids format
+outdir=`dirname $bidsfile`
+mkdir -p $outdir
+
+mv ${tempfile}.json $bidsfile.json
+mv ${tempfile}.nii.gz $bidsfile.nii.gz
+if [ -f ${tempfile}.bvec ]; then mv ${tempfile}.bvec ${bidsfile}.bvec ; fi
+if [ -f ${tempfile}.bval ]; then mv ${tempfile}.bval ${bidsfile}.bval ; fi
+
